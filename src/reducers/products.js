50f6305d2871f3ddd268a14d41afa395f8d2e88c
MariaWellander <maria.wellander@gmail.com>
2022-11-05 22:50:01 +0100

    added all code, though unsolved error with npm start

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,13 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const productData = [
-    { id: 1, title: 'Socks', price: 100, emoji: 'x' },
-    { id: 2, title: 'T-shirt', price: 200, emoji: 'Y' },
-    { id: 3, title: 'Pants', price: 350, emoji: 'Z' },
-    { id: 4, title: 'Dress', price: 500, emoji: 'W' }
+  { id: 1, title: 'Socks', price: 100, emoji: '🧦' },
+  { id: 2, title: 'T-shirt', price: 200, emoji: '👕' },
+  { id: 3, title: 'Pants', price: 350, emoji: '👖' },
+  { id: 4, title: 'Dress', price: 500, emoji: '👗' }
 ]
 
 export const products = createSlice({
-    name: 'products',
-    initialstate: productData
+  name: 'products',
+  initialstate: productData
 })
